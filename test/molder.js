282912d66e9da0f7348b3c480594836924e9ea95
bredele <olivier.wietrich@gmail.com>
2016-09-03 17:38:49 -0600

    should subsitute elements

--- test/molder.js
+++ test/molder.js
@@ -4,14 +4,22 @@ var molder = require('..')
 
 tape('replace element with other element', test => {
 	test.plan(1)
+	var parent = document.createElement('div')
+	parent.innerHTML = '<div><div>'
+
+	var component = document.createElement('button')
+
+	molder(parent.firstChild, component)
+	test.equal(parent.firstChild.outerHTML, '<button></button>')
 })
 
 
 tape('subsitute content', test => {
-	test.plan(1)
 })
 
 
 tape('subsitute content using queries', test => {
-
+  test.plan(1)
 })
+
+

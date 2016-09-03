2fc7c6bae1c3619ef03b5913b48998a233d6ddb3
bredele <olivier.wietrich@gmail.com>
2016-09-03 17:30:21 -0600

    setup test plan

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+/**
+ * [exports description]
+ * @return {[type]} [description]
+ */
+
+module.exports = function() {
+
+}
---
+++ test/molder.js
@@ -0,0 +1,17 @@
+var tape = require('tape')
+var molder = require('..')
+
+
+tape('replace element with other element', test => {
+	test.plan(1)
+})
+
+
+tape('subsitute content', test => {
+	test.plan(1)
+})
+
+
+tape('subsitute content using queries', test => {
+
+})
